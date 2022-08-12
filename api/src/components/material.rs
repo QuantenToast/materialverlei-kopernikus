eff86cac487e46ebe97a9a0b010111b0062decbe
Joel Köper <jk@joelkoeper.de>
2022-08-12 21:21:55 +0200

    fixed serialization

--- api/src/components/material.rs
+++ api/src/components/material.rs
@@ -2,12 +2,22 @@ extern crate serde;
 extern crate serde_derive;
 
 use serde_derive::{Deserialize, Serialize};
+use mongodb::bson::oid::ObjectId;
 
 #[derive(Serialize, Deserialize, Clone, PartialEq, Debug)]
-#[serde(rename_all = "camelCase")]
 pub struct Material {
     #[serde(rename = "_id", skip_serializing_if = "Option::is_none")]
-    pub id: Option<String>,
+    pub id: Option<ObjectId>,
+    pub name: String,
+    pub description: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub pic: Option<String>,
+    pub num_available: u32,
+}
+
+#[derive(Serialize, Deserialize, Clone, PartialEq, Debug)]
+pub struct MaterialRes {
+    pub id: String,
     pub name: String,
     pub description: String,
     #[serde(skip_serializing_if = "Option::is_none")]
