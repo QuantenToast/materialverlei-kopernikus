8ac6de006621fd7860d444ac17538005d3ea7c40
Joel Köper <jk@joelkoeper.de>
2022-08-14 12:42:34 +0200

    fixed formatting issue 2

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd ./frontend
 trunk build --release
-mv dist/* ../api/static/
+rm ../api/static/*
+mv dist/* index.css ../api/static/
 cd ../api
 screen -dmS mat-serve cargo run --release
