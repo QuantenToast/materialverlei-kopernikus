9b965cf590efe3f325228db2329aa22caa1b2125
Joel Köper <jk@joelkoeper.de>
2022-09-04 23:45:49 +0200

    routing

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -45,52 +45,52 @@ fn switch(routes: &Route) -> Html {
 
 #[function_component(App)]
 fn app_component() -> Html {
-    // let material = use_state(|| Vec::new());
-    // let error = use_state(|| String::new());
-    // {
-    //     let material = material.clone();
-    //     let error = error.clone();
-    //     use_effect_with_deps(
-    //         move |_| {
-    //             let material = material.clone();
-    //             let error = error.clone();
-    //             wasm_bindgen_futures::spawn_local(async move {
-    //                 let url = format!("http://81.169.248.14/api/{page}", page = 0);
-    //                 let fetched_material = Request::get(&url).send().await;
+    let material = use_state(|| Vec::new());
+    let error = use_state(|| String::new());
+    {
+        let material = material.clone();
+        let error = error.clone();
+        use_effect_with_deps(
+            move |_| {
+                let material = material.clone();
+                let error = error.clone();
+                wasm_bindgen_futures::spawn_local(async move {
+                    let url = format!("http://81.169.248.14/api/{page}", page = 0);
+                    let fetched_material = Request::get(&url).send().await;
 
-    //                 match fetched_material {
-    //                     Ok(response) => match response.json().await {
-    //                         Ok(f) => material.set(f),
-    //                         Err(e) => error.set(format!("parser: {e}")),
-    //                     },
-    //                     Err(e) => error.set(format!("server: {e}")),
-    //                 };
-    //             });
-    //             || ()
-    //         },
-    //         (),
-    //     )
-    // }
+                    match fetched_material {
+                        Ok(response) => match response.json().await {
+                            Ok(f) => material.set(f),
+                            Err(e) => error.set(format!("parser: {e}")),
+                        },
+                        Err(e) => error.set(format!("server: {e}")),
+                    };
+                });
+                || ()
+            },
+            (),
+        )
+    }
 
     html! {
-            <div id="main-container">
-                <div class="navbar">
-                    <div id="top-bar">
-                        <lable id="title">{"Materialverlei Kopernikus"}</lable>
-                        <div id="ham">
-                            <lable>{"≡"}</lable>
-                        </div>
+        <div id="main-container">
+            <div class="navbar">
+                <div id="top-bar">
+                    <lable id="title">{"Materialverlei Kopernikus"}</lable>
+                    <div id="ham">
+                        <lable>{"≡"}</lable>
                     </div>
-                    <div id="util-bar"/>
                 </div>
-                <div id="greeter"/>
-    <BrowserRouter>
-                <Switch<Route> render={Switch::render(switch)} />
-            </BrowserRouter>
-                //<MaterialListComponent materialien={(*material).clone()}/>
-                <div class="footer"/>
+                <div id="util-bar"/>
             </div>
-        }
+            <div id="greeter"/>
+                <BrowserRouter>
+                    <Switch<Route> render={Switch::render(switch)} />
+                </BrowserRouter>
+                <MaterialListComponent materialien={(*material).clone()}/>
+            <div class="footer"/>
+        </div>
+    }
 }
 
 fn main() {
